5cebde52a0771a126108b73ae7effcda5f6e819e
patildhruvi1008 <patildhruvi2103@gmail.com>
2025-04-22 15:11:56 +0530

    Update app.py

--- models/app.py
+++ models/app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load trained model
-model = pickel.load("models/branch-predictor.pkl")
+model = pickle.load("models/branch-predictor.pkl")
 st.title("Engineering Branch Predictor")
 st.write("Enter your academic details to predict your most suitable branch.")
 
